2bddcae5da895255eccb23e9d889f8ee99479189
JabimSK <6357@holbertonstudents.com>
2023-08-22 12:01:21 -0700

    0-gather_data_from_an_API.py :D

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 """ Write a Python script that, using this REST API,
 for a given employee ID, returns information about
 his/her TODO list progress. """
-//Josue Hernandez//
+
 import requests
 import sys
 
